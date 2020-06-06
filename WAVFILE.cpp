f185baa41988faee0b3cfe4b1a58b9fd9f87ebee
BoleanSwift <brytong@ukr.net>
2020-06-06 11:49:00 +0300

    new comments added, instead of propely working program

---
+++ WAVFILE.cpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+#include "WAVFILE.h"
+
+
+using namespace std;
+
+void WAVFILE::setData() {// инициализируем данные здесь
+    myData = new short int[header.subchunk2Size / header.blockAlign];
+}
+
+void WAVFILE::changeDuration(int size) {// изменяем длительность
+
+    header.chunkSize = (header.chunkSize - 36) * size + 36;//изменяем chunkSize
+    short int* tmpData = new short int[(header.subchunk2Size / header.blockAlign) * size];
+
+    int f = 0;
+
+    for (int i = 0; i < header.subchunk2Size / header.blockAlign; i++) {//изменяем данные
+        for (int d = 0; d < size; d++) {
+            tmpData[f] = myData[i];
+            f++;
+        }
+    }
+
+    myData = tmpData;
+    header.subchunk2Size = header.subchunk2Size * size;//изменяем subchunk2size
+
+
+
+}
+
---
+++ WAVFILE.h
@@ -0,0 +1,16 @@
+#ifndef OP_LR4_WAVFILE_H
+#define OP_LR4_WAVFILE_H
+
+#include <cstdint>
+#include "HEADER.h"
+
+class WAVFILE {
+public:
+    HEADER header;
+    short int* myData;// семпли
+    void setData();
+    void changeDuration(int size);
+};
+
+
+#endif //OP_LR4_WAVFILE_H
